74ca436ff30825153e7448d930fb2b7387101860
Nora Gully <nora.gully@gmail.com>
2022-10-26 08:54:22 -0600

    Add ErrorBoundary class component

---
+++ adopt-me/src/ErrorBoundary.jsx
@@ -0,0 +1,30 @@
+import { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+// EB is Class Component only - these fist 2 methods are the reason
+class ErrorBoundary extends Component {
+  state = { hasError: false };
+  static getDerivedStateFromError() {
+    // like calling setState
+    // static is an es6 thing like saying ErrorBoundary.getDerivedStateFromError()
+    // static is method you can only call on the class itself, not its instances
+    return { hasError: true }
+  }
+
+  componentDidCatch(error, info) {
+    console.error("ErrorBoundary component caught an error", error, info);
+    // send this to track.js or Sentry, your error tracking software
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <h2>
+          There was an error with this listing. <Link to="/">Click here to go back to the homepage.</Link>
+        </h2>
+      )
+    }
+  }
+}
+
+export default ErrorBoundary;
